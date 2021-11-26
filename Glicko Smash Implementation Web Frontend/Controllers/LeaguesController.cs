17a0c81f3f4f8bf62a6c0b5f55b524166ca42048
DarkBlueWool <38955974+DarkBlueWool@users.noreply.github.com>
2021-11-26 15:46:03 -0500

    Adding Create League View for setting up auth

--- Glicko Smash Implementation Web Frontend/Controllers/LeaguesController.cs
+++ Glicko Smash Implementation Web Frontend/Controllers/LeaguesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,9 +7,16 @@ using System.Threading.Tasks;
 
 namespace Glicko_Smash_Implementation_Web_Frontend.Controllers
 {
+    [Authorize]
     public class LeaguesController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
