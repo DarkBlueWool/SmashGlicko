4a1b096c7986bc5820ea2bf08f7f4251534a1b55
DarkBlueWool <38955974+DarkBlueWool@users.noreply.github.com>
2021-11-25 23:15:18 -0500

    Changed it to .Net Core 3.1

---
+++ Glicko Smash Implementation Web Frontend/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glicko_Smash_Implementation_Web_Frontend.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Glicko Smash Implementation Web Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Glicko_Smash_Implementation_Web_Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
