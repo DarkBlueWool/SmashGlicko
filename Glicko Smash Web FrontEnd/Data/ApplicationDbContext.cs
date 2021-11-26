7dca1e6deff16892f9df671fadc6a100a6878c25
DarkBlueWool <38955974+DarkBlueWool@users.noreply.github.com>
2021-11-25 23:15:18 -0500

    Changed it to .Net Core 3.1

---
+++ Glicko Smash Web FrontEnd/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glicko_Smash_Web_FrontEnd.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
