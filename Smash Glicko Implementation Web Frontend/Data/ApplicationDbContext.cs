589e267abe1d370158f62933f199935b9e8774d1
DarkBlueWool <38955974+DarkBlueWool@users.noreply.github.com>
2021-11-25 22:46:31 -0500

    Adding FrontEnd Code

---
+++ Smash Glicko Implementation Web Frontend/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smash_Glicko_Implementation_Web_Frontend.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
