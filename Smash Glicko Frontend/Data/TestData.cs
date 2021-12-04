ab5beb07f60de7ab8bf967fc5ce90aca5609094c
DarkBlueWool <38955974+DarkBlueWool@users.noreply.github.com>
2021-12-03 19:36:40 -0500

    Finishing Models, and adding test data

---
+++ Smash Glicko Frontend/Data/TestData.cs
@@ -0,0 +1,36 @@
+﻿using Smash_Glicko_Frontend.Models;
+
+namespace Smash_Glicko_Frontend.Data
+{
+    public class TestEvents
+    {
+        private static List<EventModel> Events = new List<EventModel>()
+        {
+            new EventModel(0, 32),
+            new EventModel(1, 64),
+            new EventModel(2, 35),
+            new EventModel(3, 89)
+        };
+
+        public static TimeFrameModel timeFrame = new TimeFrameModel(Events, new Dictionary<uint, PlayerModel>());
+    }
+    public class TestData
+    {
+        private static List<EventModel> _events = new List<EventModel>();
+        private static List<TimeFrameModel> _timeFrames = new List<TimeFrameModel> { new TimeFrameModel(_events, new Dictionary<uint, PlayerModel>()) };
+        public static List<LeagueModel> Leagues = new List<LeagueModel>() {
+            new LeagueModel("A League", "SSBU", _timeFrames, DateTime.Now, new TimeSpan(7,0,0,0), true),
+            new LeagueModel("B League", "SSBU", _timeFrames, DateTime.Now, new TimeSpan(7,0,0,0), true),
+            new LeagueModel("C League", "SSBU", _timeFrames, DateTime.Now, new TimeSpan(7,0,0,0), true),
+            new LeagueModel("D League", "SSBU", _timeFrames, DateTime.Now, new TimeSpan(7,0,0,0), true),
+            new LeagueModel("E League", "SSBU", _timeFrames, DateTime.Now, new TimeSpan(7,0,0,0), true),
+            new LeagueModel("F League", "SSBU", _timeFrames, DateTime.Now, new TimeSpan(7,0,0,0), true),
+            new LeagueModel("G League", "SSBU", _timeFrames, DateTime.Now, new TimeSpan(7,0,0,0), true),
+            new LeagueModel("H League", "SSBU", _timeFrames, DateTime.Now, new TimeSpan(7,0,0,0), true),
+            new LeagueModel("I League", "SSBU", _timeFrames, DateTime.Now, new TimeSpan(7,0,0,0), true),
+            new LeagueModel("J League", "SSBU", _timeFrames, DateTime.Now, new TimeSpan(7,0,0,0), true),
+            new LeagueModel("K League", "SSBU", _timeFrames, DateTime.Now, new TimeSpan(7,0,0,0), true),
+            new LeagueModel("L League", "SSBU", _timeFrames, DateTime.Now, new TimeSpan(7,0,0,0), true),
+        };
+    }
+}
--- Smash Glicko Frontend/Models/LeagueModels.cs
+++ Smash Glicko Frontend/Models/LeagueModels.cs
@@ -12,4 +12,116 @@ namespace Smash_Glicko_Frontend.Models
 
         public bool IsPublic { get; set; }
     }
+
+    public class LeagueModel
+    {
+        public string LeagueName { get; set; }
+
+        public string LeagueGame { get; set; }
+
+        public List<TimeFrameModel> TimeFrames { get; set; }
+
+        public DateTime InitalTimeFrameStart { get; set; }
+
+        public TimeSpan TimeFrameSpan { get ; set; }
+
+        public LeagueModel(string Name, string Game, List<TimeFrameModel> timeFrames, DateTime initialTimeFrameStart, TimeSpan timeFrameSpan, bool isPublic)
+        {
+            LeagueName = Name;
+            LeagueGame = Game;
+            TimeFrames = timeFrames;
+            InitalTimeFrameStart = initialTimeFrameStart;
+            TimeFrameSpan = timeFrameSpan;
+            IsPublic = isPublic;
+        }
+
+        public bool IsPublic { get; set; }
+    }
+
+    public class TimeFrameModel
+    {
+        public Dictionary<uint, PlayerModel> Players = new Dictionary<uint, PlayerModel>();
+
+        public uint EventCount;
+
+        public List<EventModel> Events { get; set;}
+
+        public TimeFrameModel(List<EventModel> IniEvents, Dictionary<uint, PlayerModel> IniPlayers)
+        {
+            Dictionary<uint, PlayerModel> AllPlayers = IniPlayers;
+            Events = IniEvents;
+        }
+
+        public TimeFrameModel StartNextTimeFrame()
+        {
+            Dictionary<uint, PlayerModel> UpdatedPlayers = new Dictionary<uint, PlayerModel>();
+
+            //Temp shit
+            UpdatedPlayers = Players;
+
+            TimeFrameModel NewFrame = new TimeFrameModel(new List<EventModel>(), UpdatedPlayers);
+            return NewFrame;
+        }
+
+        public void AddPlayers(Dictionary<uint, PlayerModel> NewPlayers)
+        {
+            if (Players.Count == 0)
+            {
+                Players = NewPlayers;
+            } else
+            {
+                foreach (KeyValuePair<uint, PlayerModel> Player in NewPlayers)
+                {
+                    if (!Players.Keys.Contains(Player.Key))
+                    {
+                        Players.Add(Player.Key, Player.Value);
+                    }
+                }
+            }
+        }
+    }
+
+    //UNFINISHED
+    public class PlayerModel
+    {
+        public long Id { get; set; }
+
+        public bool IsNew = true;
+
+        public PlayerModel(uint PlayerID)
+        {
+            Id = PlayerID;
+        }
+    }
+    public class EventModel
+    {
+        public uint EventID;
+
+        public uint PlayerCount;
+
+        public List<SetModel> Sets = new List<SetModel>();
+
+        public EventModel(uint eventID, uint playerCount)
+        {
+            EventID = eventID;
+            PlayerCount = playerCount;
+        }
+        public EventModel(uint eventID, uint playerCount, List<SetModel> sets)
+        {
+            EventID = eventID;
+            PlayerCount = playerCount;
+            Sets = sets;
+        }
+    }
+
+    public class SetModel
+    {
+        public uint Player1ID { get; set; }
+
+        public uint Player2ID { get; set; }
+
+        public uint Player1Wins { get; set; }
+
+        public uint Player2Wins { get; set; }
+    }
 }
